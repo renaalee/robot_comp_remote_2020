1dfb404bd64b6759e879afca38f2e2cf6878295b
Rena Aerey Lee <39202458+renaalee@users.noreply.github.com>
2020-05-10 16:40:52 -0700

    Add files via upload
    code for milestone 3

---
+++ Milestone 3/Lee_milestone3.ino
@@ -0,0 +1,39 @@
+// Remote Comp Milestone 3
+// Rena A. Lee Spring 2020
+
+#include <Wire.h>
+#include <ZumoShield.h>
+
+ZumoReflectanceSensorArray linesensors(QTR_NO_EMITTER_PIN);
+ZumoMotors motors;
+
+void setup() {
+  int i;
+  int spin_direction = 1;
+  motors.setSpeeds(90*spin_direction, -90*spin_direction);
+  for(i = 0; i<100; i++){
+    linesensors.calibrate();
+    if(i%50 == 25){ // every 50 loops, starting on loop 25...
+      spin_direction = -spin_direction;
+      motors.setSpeeds(90*spin_direction, -90*spin_direction);
+    }
+    delay(20);
+  }
+  motors.setSpeeds(0,0);
+  delay(300);
+}
+
+unsigned int sensor_vals[6];
+int BASE_SPEED = 300;
+double PROPORTION_GAIN = 0.3;
+double DERIVATIVE_GAIN = 3;
+int last_error = 0;
+void loop() {
+  int line_posn = linesensors.readLine(sensor_vals);
+  int error = line_posn - 2500;
+  int delta_e = error - last_error;
+  int left_speed = BASE_SPEED + PROPORTION_GAIN*error + DERIVATIVE_GAIN*delta_e;
+  int right_speed = BASE_SPEED + -PROPORTION_GAIN*error + -DERIVATIVE_GAIN*delta_e;
+  last_error = error;\
+  motors.setSpeeds(left_speed, right_speed);
+}
